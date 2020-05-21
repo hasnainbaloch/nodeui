559b40faea30a3fc087b199028f8c7352311a27c
Hasnain Ali Baloch <hasnainalibaloch@outlook.com>
2020-05-22 02:40:45 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { readFile }  = require('fs').promises;
+
+const app = express();
+
+app.get('/', async (request, response) => {
+    response.send( await readFile('./index.html') );
+})
+
+
+app.listen('2300', () => {
+    console.log("App is live");
+})
